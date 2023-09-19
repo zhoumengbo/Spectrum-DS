1230b2deb3ab64b3a4ea1c55b2120fd0c0b2ce06
zhoumengbo <zhoumengbo1106@163.com>
2023-09-19 16:12:43 +0800

    update

--- similarityAnalysis.py
+++ similarityAnalysis.py
@@ -5,28 +5,44 @@ import similarityCoefficient
 matplotlib.use('TkAgg')
 
 
-def Coefficient(failure_testcase_folder, html_folder):
+def Coefficient(failure_testcase_folder, html_folder, fault_loc, n):
     result_dic = similarity_measure(failure_testcase_folder, html_folder)
-    for k, v in result_dic.items():
-        sus = similarityCoefficient.Tarantula(v[0], v[1], v[2], v[3])
-        result_dic[k] = sus
-    sort_list = sorted(result_dic.items(), key=lambda x: x[1], reverse=True)
-    print(sort_list)
+    print(result_dic)
+    for coefficient in similarityCoefficient.coefficient_list:
+        sus_dic = {}
+        for k, v in result_dic.items():
+            sus = similarityCoefficient.select_coefficient(coefficient, v[0], v[1], v[2], v[3])
+            sus_dic[k] = sus
 
-    all_value = {}
-    for v in result_dic.values():
-        if v in all_value:
-            all_value[v] = all_value.get(v) + 1
-        else:
-            all_value[v] = 1
-    print('Suspiciousness: {0}'.format(sorted(all_value.items(), key=lambda x: x[0], reverse=True)))
+        all_sus = {}
+        for v in sus_dic.values():
+            if v in all_sus:
+                all_sus[v] = all_sus.get(v) + 1
+            else:
+                all_sus[v] = 1
 
-    sort_dic = {k: v for k, v in sort_list[:30]}
-    fig, ax = plt.subplots()
-    ax.bar(sort_dic.keys(), sort_dic.values())
-    plt.xlabel('Statement')
-    plt.ylabel('Suspiciousness')
-    plt.show()
+        g = 0
+        e = 0
+        fault_sus = sus_dic.get(fault_loc)
+        for sus, num in all_sus.items():
+            if sus > fault_sus:
+                g += num
+            elif sus == fault_sus:
+                e += num
+        EXAM = (g + e/2) / n
+        print('****************************************')
+        print('Coefficient: {0}'.format(coefficient))
+        print('Suspiciousness: {0}'.format(sorted(all_sus.items(), key=lambda x: x[0], reverse=True)))
+        print('Fault statement: {0}, EXAM: {1}/{2}={3}'.format(fault_sus, g + e/2, n, "%.4f%%" % (EXAM * 100)))
+
+    # # Histogram display
+    # sort_list = sorted(result_dic.items(), key=lambda x: x[1], reverse=True)
+    # sort_dic = {k: v for k, v in sort_list[:30]}
+    # fig, ax = plt.subplots()
+    # ax.bar(sort_dic.keys(), sort_dic.values())
+    # plt.xlabel('Statement')
+    # plt.ylabel('Suspiciousness')
+    # plt.show()
 
 
 def similarity_measure(failure_testcase_folder, html_folder):
@@ -55,7 +71,10 @@ def similarity_measure(failure_testcase_folder, html_folder):
 
 
 if __name__ == '__main__':
+    html_folder_path = "/home/zmb/project/Fault_Localization/Benchmark-DS/Zookeeper/Experiment/v1/ZK_1419/htmlReport"
     failure_testcase_folder_path = ("/home/zmb/project/Fault_Localization/Benchmark-DS/Zookeeper/Experiment/v1"
                                     "/ZK_1419/htmlReport/1.test.FLEPredicateTest")
-    html_folder_path = "/home/zmb/project/Fault_Localization/Benchmark-DS/Zookeeper/Experiment/v1/ZK_1419/htmlReport"
-    Coefficient(failure_testcase_folder_path, html_folder_path)
+    fault_loc = 'org.apache.zookeeper.server.quorum.FastLeaderElection.244'
+    statement_num = 23630
+
+    Coefficient(failure_testcase_folder_path, html_folder_path, fault_loc, statement_num)
--- similarityCoefficient.py
+++ similarityCoefficient.py
@@ -1,3 +1,6 @@
+coefficient_list = ['Tarantula', 'Ochiai', 'Jaccard', 'AMPLE', 'Kulczynski', 'D2', 'OP2', 'SBI', 'Ochiai2', 'Zoltar']
+
+
 def Tarantula(N_cf, N_uf, N_cs, N_us):
     numerator = N_cf / (N_cf + N_uf)
     denominator = numerator + (N_cs / (N_cs + N_us))
@@ -16,7 +19,28 @@ def Ochiai(N_cf, N_uf, N_cs, N_us):
         return numerator / denominator
 
 
-def D(N_cf, N_uf, N_cs, N_us):
+def Jaccard(N_cf, N_uf, N_cs, N_us):
+    numerator = N_cf
+    denominator = N_cf + N_cs + N_uf
+    if denominator == 0.0:
+        return 0.0
+    else:
+        return numerator / denominator
+
+
+def AMPLE(N_cf, N_uf, N_cs, N_us):
+    if N_cf + N_uf == 0.0:
+        item1 = 0.0
+    else:
+        item1 = N_cf / (N_cf + N_uf)
+    if (N_cs + N_us) == 0.0:
+        item2 = 0.0
+    else:
+        item2 = N_cs / (N_cs + N_us)
+    return abs(item1 - item2)
+
+
+def Kulczynski(N_cf, N_uf, N_cs, N_us):
     numerator = N_cf
     denominator = N_cs + N_uf
     if denominator == 0.0:
@@ -32,3 +56,65 @@ def D2(N_cf, N_uf, N_cs, N_us):
         return 0.0
     else:
         return numerator / denominator
+
+
+def OP2(N_cf, N_uf, N_cs, N_us):
+    item1 = N_cf
+    item2 = N_cs / (N_cs + N_us + 1)
+    return item1 - item2
+
+
+def SBI(N_cf, N_uf, N_cs, N_us):
+    item1 = 1
+    if N_cs + N_cf == 0.0:
+        item2 = 0.0
+    else:
+        item2 = N_cs / (N_cs + N_cf)
+    return item1 - item2
+
+
+def Ochiai2(N_cf, N_uf, N_cs, N_us):
+    numerator = N_cf * N_us
+    denominator = ((N_cf + N_cs) * (N_us + N_uf) * (N_cf + N_uf) * (N_cs + N_us)) ** 0.5
+    if denominator == 0.0:
+        return 0.0
+    else:
+        return numerator / denominator
+
+
+def Zoltar(N_cf, N_uf, N_cs, N_us):
+    numerator = N_cf
+    if N_cf == 0.0:
+        item = 0.0
+    else:
+        item = (10000 * N_uf * N_cs) / N_cf
+    denominator = N_cf + N_uf + N_cs + item
+    if denominator == 0.0:
+        return 0.0
+    else:
+        return numerator / denominator
+
+
+def select_coefficient(name, N_cf, N_uf, N_cs, N_us):
+    if name == coefficient_list[0]:
+        return Tarantula(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[1]:
+        return Ochiai(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[2]:
+        return Jaccard(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[3]:
+        return AMPLE(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[4]:
+        return Kulczynski(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[5]:
+        return D2(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[6]:
+        return OP2(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[7]:
+        return SBI(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[8]:
+        return Ochiai2(N_cf, N_uf, N_cs, N_us)
+    elif name == coefficient_list[9]:
+        return Zoltar(N_cf, N_uf, N_cs, N_us)
+    else:
+        print("Error in select_coefficient")
